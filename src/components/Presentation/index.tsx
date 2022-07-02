7defd2f7b87f37cddbd75d62d84b0161bfae3e50
eduwr <eduwric@gmail.com>
2022-07-02 12:37:13 -0300

    feat: presentation component

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -1,31 +1,21 @@
-import Image from "next/image";
+
 import { Translated } from "../Translated";
 import { BaseButton } from "../BaseButton";
+import { ProfilePicture } from "./ProfilePicture";
 
 export const Presentation = () => (
-  <section
-    className="flex flex-col md:flex-row justify-center gap-8 items-center py-8 md:py-16 md:px-8 w-full md:h-1/2-screen md:min-h-[26rem] relative bg-secondary">
-    <div className="h-96 w-96 rounded-full relative overflow-hidden">
-      <Image
-        src="/assets/images/profile.png"
-        alt="Yoga"
-        layout="fill"
-        objectFit="cover"
-      />
-    </div>
-    <div className="md:w-1/2 px-5 h-full flex flex-col relative">
-      <h3 className="lg:text-5xl text-4xl text-center md:text-left text-primary font-bold mb-5 uppercase">
-        <Translated textKey="home.presentation.title"/>
-      </h3>
-      <p className="text-2xl text-secondary-content font-thin text-center md:text-left">
-        <Translated textKey="home.presentation.description"/>
+  <section className="bg-primary-content border-2 border-white px-8 md:px-24">
+    <div className="max-w-1/2">
+      <h1 className="text-primary text-5xl leading-relaxed uppercase font-bold">
+        <Translated textKey="home.presentation.title" />
+      </h1>
+      <p className="text-2xl text-tertiary mt-3 mb-8">
+        <Translated textKey="home.presentation.description" />
       </p>
-      <BaseButton outline style={{
-        alignSelf: "flex-end",
-        marginTop: "2rem"
-      }}>
-        <Translated textKey="home.presentation.button"/>
+      <BaseButton outline>
+        <Translated textKey={"home.presentation.button"} />
       </BaseButton>
     </div>
+    <ProfilePicture />
   </section>
 );
