f71481d9b4045ab7d3682ec16fc7cea4c70955ff
eduwr <eduwric@gmail.com>
2022-07-02 13:42:37 -0300

    feat: finalize Presentation section

--- src/components/Presentation/ProfilePicture.tsx
+++ src/components/Presentation/ProfilePicture.tsx
@@ -1,6 +1,6 @@
 export const ProfilePicture = () => {
   return <div>
-    <svg width="418" height="491" viewBox="0 0 418 491" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="540" height="650" viewBox="0 0 418 491" fill="none" xmlns="http://www.w3.org/2000/svg">
       <defs>
         <mask id="svgmask">
           <path d="M398.282 380.314C368.992 467.508 320.76 503.13 253.587 487.18C186.414 471.23 119.631 427.633 53.2389 356.388C-13.1532 285.144 -17.4491 203.798 40.3511 112.35C98.1512 20.9024 161.419 -14.7198 230.154 5.48383C298.89 25.6874 351.417 71.4114 387.738 142.656C424.058 213.9 427.573 293.119 398.282 380.314Z" fill="white"/>
@@ -8,8 +8,11 @@ export const ProfilePicture = () => {
         <filter id="filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="linearRGB">
           <feColorMatrix type="saturate" values="-0" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="colormatrix"/>
         </filter>
+        <radialGradient id="RadialGradient">
+          <stop offset="0%" stopColor="red"/>
+          <stop offset="100%" stopColor="blue"/>
+        </radialGradient>
       </defs>
-
       <image
         xmlns="http://www.w3.org/1999/xlink"
         href="/assets/images/profile.png"
@@ -17,7 +20,6 @@ export const ProfilePicture = () => {
         height="491"
         filter="url(#filter)"
       />
-
     </svg>
   </div>
 }
--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -4,8 +4,8 @@ import { BaseButton } from "../BaseButton";
 import { ProfilePicture } from "./ProfilePicture";
 
 export const Presentation = () => (
-  <section className="bg-primary-content border-2 border-white px-8 md:px-24">
-    <div className="max-w-1/2">
+  <section className="flex justify-between bg-primary-content px-8 md:px-24">
+    <div className="flex flex-col justify-center max-w-1/2">
       <h1 className="text-primary text-5xl leading-relaxed uppercase font-bold">
         <Translated textKey="home.presentation.title" />
       </h1>
@@ -16,6 +16,9 @@ export const Presentation = () => (
         <Translated textKey={"home.presentation.button"} />
       </BaseButton>
     </div>
-    <ProfilePicture />
+    <div className="relative">
+      <div className="bg-gradient-to-tl from-primary-content absolute top-0 right-0 left-0 bottom-0"></div>
+      <ProfilePicture />
+    </div>
   </section>
 );
