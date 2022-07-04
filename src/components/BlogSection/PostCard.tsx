67ae47982cc4c0f60bcc677fc1cfbf61a481a574
eduwr <eduwric@gmail.com>
2022-07-04 11:17:53 -0300

    feat: add 3d aspect to texts on card

--- src/components/BlogSection/PostCard.tsx
+++ src/components/BlogSection/PostCard.tsx
@@ -27,13 +27,18 @@ export const PostCard = ({ post }: Props) => {
         target="_blank"
         style={{
           transform: `perspective(900px) rotateY(${-rotationCoordinates[0]}deg) rotateX(${rotationCoordinates[1]}deg)`,
-          filter: `drop-shadow(${rotationCoordinates[0]}px ${rotationCoordinates[1]}px 20px rgb(0 0 0 / 0.5))`
+          filter: `drop-shadow(${rotationCoordinates[0]}px ${rotationCoordinates[1]}px 20px rgb(0 0 0 / 0.5))`,
+          transformStyle: "preserve-3d"
         }}
       >
         <li
-          className={`flex flex-col justify-end w-80 h-80 xl:w-96 xl:h-96 rounded-2xl relative overflow-hidden`}>
+          className={`flex flex-col justify-end w-80 h-80 xl:w-96 xl:h-96 rounded-2xl relative `}>
           <div
-            className="absolute flex flex-col justify-end p-3 top-0 left-0 right-0 bottom-0 z-30"
+            className="absolute flex flex-col justify-end p-3 top-0 left-0 right-0 bottom-0 z-30 rounded-2xl"
+            style={{
+              transform: `${rotationCoordinates.some(c => c) ? `perspective(900px) translateZ(80px) translateX(20px) translateY(-10px) rotateY(${-rotationCoordinates[0]}deg) rotateX(${rotationCoordinates[1]}deg)` : ''}`,
+
+            }}
           >
             <span className="text-secondary font-thin text-sm">{added.format("Do MMMM YYYY")}</span>
 
@@ -45,14 +50,16 @@ export const PostCard = ({ post }: Props) => {
             style={{
               background: "radial-gradient(#2b2b2b22, #2b2b2b)"
             }}
-            className="absolute top-0 bottom-0 left-0 right-0 z-20"
+            className="absolute top-0 bottom-0 left-0 right-0 z-20 rounded-2xl overflow-hidden"
           />
-          <Image
-            src={post.coverImage}
-            layout="fill"
-            objectFit="cover"
-            alt={post.title}
-          />
+          <div className="absolute top-0 bottom-0 left-0 right-0 rounded-2xl overflow-hidden">
+            <Image
+              src={post.coverImage}
+              layout="fill"
+              objectFit="cover"
+              alt={post.title}
+            />
+          </div>
         </li>
       </a>
     </Link>
