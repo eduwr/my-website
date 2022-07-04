73ce119d58976dc15133d3c3a56b6045df9bc797
eduwr <eduwric@gmail.com>
2022-07-03 22:44:58 -0300

    feat: fix lang texts

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   i18n: {
     locales: ['en-US', 'pt-BR'],
-    defaultLocale: 'pt-BR'
+    defaultLocale: 'en-US'
   },
   images: {domains: ["cdn.hashnode.com"]}
 
