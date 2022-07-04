7d897490af70557b21bf31eeec4c6e1bb7fe1ce1
eduwr <eduwric@gmail.com>
2022-07-03 23:35:49 -0300

    feat: presentation responsive

--- src/components/Presentation/ProfilePicture.tsx
+++ src/components/Presentation/ProfilePicture.tsx
@@ -1,25 +1,34 @@
+import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+import { BREAKPOINTS } from "../../utils/breakpoints";
+
 export const ProfilePicture = () => {
-  return <div>
-    <svg width="540" height="650" viewBox="0 0 418 491" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <mask id="svgmask">
-          <path d="M398.282 380.314C368.992 467.508 320.76 503.13 253.587 487.18C186.414 471.23 119.631 427.633 53.2389 356.388C-13.1532 285.144 -17.4491 203.798 40.3511 112.35C98.1512 20.9024 161.419 -14.7198 230.154 5.48383C298.89 25.6874 351.417 71.4114 387.738 142.656C424.058 213.9 427.573 293.119 398.282 380.314Z" fill="white"/>
-        </mask>
-        <filter id="filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="linearRGB">
-          <feColorMatrix type="saturate" values="-0" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="colormatrix"/>
-        </filter>
-        <radialGradient id="RadialGradient">
-          <stop offset="0%" stopColor="red"/>
-          <stop offset="100%" stopColor="blue"/>
-        </radialGradient>
-      </defs>
-      <image
-        xmlns="http://www.w3.org/1999/xlink"
-        href="/assets/images/profile.png"
-        mask="url(#svgmask)"
-        height="491"
-        filter="url(#filter)"
-      />
-    </svg>
-  </div>
+  const {width} = useWindowDimensions();
+
+  const breakpoint = width && width <= BREAKPOINTS.EXTRA_LARGE;
+
+  return (
+    <div>
+      <svg width={breakpoint ? "378" : "540"} height={breakpoint ? "455" : "650"} viewBox="0 0 418 491" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <mask id="svgmask">
+            <path d="M398.282 380.314C368.992 467.508 320.76 503.13 253.587 487.18C186.414 471.23 119.631 427.633 53.2389 356.388C-13.1532 285.144 -17.4491 203.798 40.3511 112.35C98.1512 20.9024 161.419 -14.7198 230.154 5.48383C298.89 25.6874 351.417 71.4114 387.738 142.656C424.058 213.9 427.573 293.119 398.282 380.314Z" fill="white"/>
+          </mask>
+          <filter id="filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="linearRGB">
+            <feColorMatrix type="saturate" values="-0" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="colormatrix"/>
+          </filter>
+          <radialGradient id="RadialGradient">
+            <stop offset="0%" stopColor="red"/>
+            <stop offset="100%" stopColor="blue"/>
+          </radialGradient>
+        </defs>
+        <image
+          xmlns="http://www.w3.org/1999/xlink"
+          href="/assets/images/profile.png"
+          mask="url(#svgmask)"
+          height="491"
+          filter="url(#filter)"
+        />
+      </svg>
+    </div>
+  )
 }
--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -4,19 +4,20 @@ import { BaseButton } from "../BaseButton";
 import { ProfilePicture } from "./ProfilePicture";
 
 export const Presentation = () => (
-  <section className="flex justify-between bg-primary-content px-8 md:px-24">
-    <div className="flex flex-col justify-center max-w-1/2">
-      <h1 className="text-primary text-5xl leading-relaxed uppercase font-bold">
+  <section className="flex justify-between bg-primary-content px-8 md:px-24 pb-8">
+    <div className="flex flex-col justify-center md:max-w-1/2">
+      <h1 className="text-primary text-4xl xl:text-5xl leading-relaxed uppercase font-bold">
         <Translated textKey="home.presentation.title" />
       </h1>
-      <p className="text-2xl text-tertiary mt-3 mb-8">
+      <p className="text-xl xl:text-2xl text-tertiary mt-3 mb-8">
         <Translated textKey="home.presentation.description" />
       </p>
       <BaseButton outline>
         <Translated textKey={"home.presentation.button"} />
       </BaseButton>
     </div>
-    <div className="relative">
+    {/* Profile Image Container */}
+    <div className="hidden relative md:flex">
       <div className="bg-gradient-to-tl from-primary-content absolute top-0 right-0 left-0 bottom-0"></div>
       <ProfilePicture />
     </div>
