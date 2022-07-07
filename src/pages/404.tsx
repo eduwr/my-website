4416c2865b166e39e69e24e761390bd56a0fbb72
eduwr <eduwric@gmail.com>
2022-07-06 21:23:17 -0300

    feat: minimal style to 500 and 404 pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,19 @@
+import Head from "next/head";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div
+      className="flex flex-col items-center justify-center text-tertiary"
+    >
+      <Head>
+        <title>Eduardo Wronscki | 404</title>
+        <meta name="description" content="Eduardo Wronscki - Full-Stack Developer"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <h1 className="text-6xl mb-5 font-bold">404</h1>
+      <h1 className="text-4xl font-bold">Oops! Page Not Found.</h1>
+      <p className="mt-8">The page you&apos;re looking for is out of range.</p>
+    </div>
+  )
 }
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,3 +1,19 @@
+import Head from "next/head";
+
 export default function Custom500() {
-  return <h1>500 - Internal server error</h1>;
+  return (
+    <div
+      className="flex flex-col items-center justify-center text-tertiary"
+    >
+      <Head>
+        <title>Eduardo Wronscki | 500</title>
+        <meta name="description" content="Eduardo Wronscki - Full-Stack Developer"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <h1 className="text-6xl mb-5 font-bold">500</h1>
+      <h1 className="text-4xl font-bold">Oops! Something went wrong.</h1>
+      <p className="mt-8">Sorry about that. You can try again later.</p>
+    </div>
+  );
 }
